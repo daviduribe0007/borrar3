312f5d6189c057dcf538f7f48ece22e2a7e164ec
David uribe <Daviduribe0007@gmail.com>
2021-05-16 12:06:55 -0500

    Se crea el projectos y se agregan las dependencias iniciales al pom

---
+++ src/main/java/com/example/card/web/flux/CardWebFluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.card.web.flux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardWebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardWebFluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/card/web/flux/CardWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.card.web.flux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardWebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
